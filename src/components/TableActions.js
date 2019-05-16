04051b3981856fd9d429ad3bdbe13f0767e30ab9
Andrey Stolpner <andrey.stolpner@neotech.by>
2019-05-16 17:57:04 +0300

    added sematic ui to actions table btn

--- src/components/TableActions.js
+++ src/components/TableActions.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import { Button } from 'semantic-ui-react';
 
+import './TableActions.css'
+
 class TableActions extends Component {
   render() {
     if (!this.props.isShow) return null;
     return (
-      <div>
-        <Button onClick={ this.props.changeStatusTasks }>Change Status Tasks</Button>
-        <Button onClick={ this.props.deleteSelectedItems }>Delete</Button>
-        <Button onClick={ this.props.clearSelectedItems }>Clear Selected</Button>
+      <div className="tableActions">
+        <Button
+          basic
+          color="green"
+          onClick={ this.props.changeStatusTasks }
+        >
+          Change Status Tasks
+        </Button>
+        <Button
+          basic
+          color="red"
+          onClick={ this.props.deleteSelectedItems }
+        >
+          Delete
+        </Button>
+        <Button
+          basic
+          onClick={ this.props.clearSelectedItems }
+        >
+          Clear Selected
+        </Button>
       </div>
     )
   }
